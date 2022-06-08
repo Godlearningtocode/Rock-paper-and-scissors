7f4868f07fb0ad068afe4c179c2bcd6fd2ec24ce
Shivam <shivam.ayush.agarwal@gmail.com>
2022-06-09 02:08:55 +0530

    fixed the prompt issue

---
+++ project.js
@@ -0,0 +1,68 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+
+
+let computerSelection = computerPlay();
+
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+
+let playerWinRound = "You Win!";
+let playerLoseRound = "You lose!"
+
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "Draw!";
+        } else if (computerSelection === "paper") {
+            return playerLoseRound;
+        } else {
+            return playerWinRound;
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return playerWinRound;
+        } else if (computerSelection === "paper") {
+            return "Draw!";
+        } else {
+            return playerLoseRound;
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return playerLoseRound;
+        } else if (computerSelection === "paper") {
+            return playerWinRound;
+        } else {
+            return "Draw!";
+        }
+    }
+}
+
+
+
+    for (i=0; i<5; i++) {
+        console.log(computerPlay);
+        let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        gameScore(roundResult);
+        console.log("Your score is: " + playerScore);
+        console.log("Computer's score is: " + computerScore);
+    }
+
+    function gameScore(result) {
+        if (result === playerWinRound) {
+            playerScore++;
+        } else if (result === playerLoseRound) {
+            computerScore++;
+        } else {
+            draw++;
+        }
+    }
